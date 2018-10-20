edf30fe2ae8feb4a29a6e573e4b84ab35c2de2e6
Anatolij <java1cprog@yandex.ru>
2018-10-20 11:55:47 +0200

    add implementation of `Default` trait and change `new` function

--- episode/1/greeter/src/lib.rs
+++ episode/1/greeter/src/lib.rs
@@ -1,11 +1,13 @@
 use std::fmt;
+use std::default;
 
 #[derive(Debug)]
+#[allow(dead_code)]
 enum Language {
     English,
     German,
+    Russian,
 }
-
 #[derive(Debug)]
 struct Greeter {
     language: Language,
@@ -16,30 +18,54 @@ impl fmt::Display for Greeter {
         let greeting = match self.language {
             Language::English => "Hello",
             Language::German => "Hallo",
+            Language::Russian => "Привет",
         };
-        write!(f, "{} Rust", greeting)
+        write!(f, "{} Rust!", greeting)
     }
 }
 
-impl Greeter {
-    fn new() -> Greeter {
-        Greeter {
-            language: Language::English,
+impl default::Default for Greeter {
+    fn default()->Greeter{
+        Greeter{
+            language:Language::English,
         }
     }
+}
 
-    fn with_language(mut self, language: Language) -> Greeter {
+#[allow(dead_code)]
+impl Greeter {
+    fn new(language: Language) -> Greeter {
+        Greeter { language }
+    }
+
+    pub fn with_language(mut self, language: Language) -> Greeter {
         self.language = language;
         self
     }
 }
 
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
-    fn it_works() {
-        let greeter = Greeter::new().with_language(Language::German);
-        assert_eq!(format!("{}", greeter), "Hallo Rust");
+    fn it_works1() {
+        let greeter:Greeter = Default::default();
+        assert_eq!(format!("{}",greeter), "Hello Rust!");
+
     }
+
+    #[test]
+    fn it_works2() {
+        let greeter = Greeter::new(Language::German);
+        assert_eq!(format!("{}",greeter), "Hallo Rust!");
+    }
+    #[test]
+    fn it_works3() {
+        let mut  greeter = Greeter::new(Language::German);
+        greeter = greeter.with_language(Language::Russian);
+        assert_eq!(format!("{}",greeter), "Привет Rust!");
+    }
+
 }
